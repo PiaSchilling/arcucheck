b21e0414cdfffaba0a21c0b6d373b82399bd3b9c
Pia Schilling <ps149@hdm-stuttgart.de>
2024-05-22 11:13:22 +0200

    feat: get plantUML parser running
    Very prototypical state which has to be revised

--- arcucheck/src/main/kotlin/Main.kt
+++ arcucheck/src/main/kotlin/Main.kt
@@ -1,32 +1,8 @@
-import com.shuzijun.plantumlparser.cli.options.CliOptionsHolder
-import com.shuzijun.plantumlparser.cli.options.ParserConfigUpdater
-import com.shuzijun.plantumlparser.core.ParserConfig
-import com.shuzijun.plantumlparser.core.ParserProgram
-import org.apache.commons.cli.CommandLine
-import org.apache.commons.cli.CommandLineParser
-import org.apache.commons.cli.DefaultParser
-import org.apache.commons.cli.Options
-import java.util.function.Consumer
+import parse.impl.UMLParser
+
 
 
 fun main(args: Array<String>) {
-    try {
-        val options: Options = CliOptionsHolder.createOptions()
-        val parser: CommandLineParser = DefaultParser()
-        val cmd: CommandLine = parser.parse(options, args)
-
-        // Updated ParserConfig
-        val config = ParserConfig()
-        CliOptionsHolder.getParserConfigUpdater()
-            .forEach(Consumer { parserConfigUpdater: ParserConfigUpdater ->
-                parserConfigUpdater.updateConfig(
-                    config,
-                    cmd
-                )
-            })
-        val program = ParserProgram(config)
-        program.execute()
-    } catch (ex: Exception) {
-        ex.printStackTrace()
-    }
+   val parser = UMLParser();
+    parser.parseCode(args);
 }
