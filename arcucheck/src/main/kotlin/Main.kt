76f696710ec734e4a1011893d806a42e26505abb
Pia Schilling <ps149@hdm-stuttgart.de>
2024-05-22 10:48:32 +0200

    feat: get plantUML parser running
    Very prototypical state which has to be revised

--- arcucheck/src/main/kotlin/Main.kt
+++ arcucheck/src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.shuzijun.plantumlparser.cli.options.CliOptionsHolder
+import com.shuzijun.plantumlparser.cli.options.ParserConfigUpdater
+import com.shuzijun.plantumlparser.core.ParserConfig
+import com.shuzijun.plantumlparser.core.ParserProgram
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.CommandLineParser
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Options
+import java.util.function.Consumer
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main(args: Array<String>) {
+    try {
+        val options: Options = CliOptionsHolder.createOptions()
+        val parser: CommandLineParser = DefaultParser()
+        val cmd: CommandLine = parser.parse(options, args)
+
+        // Updated ParserConfig
+        val config = ParserConfig()
+        CliOptionsHolder.getParserConfigUpdater()
+            .forEach(Consumer { parserConfigUpdater: ParserConfigUpdater ->
+                parserConfigUpdater.updateConfig(
+                    config,
+                    cmd
+                )
+            })
+        val program = ParserProgram(config)
+        program.execute()
+    } catch (ex: Exception) {
+        ex.printStackTrace()
+    }
 }
