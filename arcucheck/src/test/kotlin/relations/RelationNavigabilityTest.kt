46cbdb9ee974a3367f34e45ab8883ad23d90db8c
Pia Schilling <ps149@hdm-stuttgart.de>
2024-08-01 15:07:27 +0200

    feat: add test for navigable associations

---
+++ arcucheck/src/test/kotlin/relations/RelationNavigabilityTest.kt
@@ -0,0 +1,70 @@
+package relations
+
+import control.api.Controller
+import control.di.controlModule
+import core.constants.TEST
+import core.di.coreModule
+import core.model.deviation.DeviationLevel
+import core.model.deviation.DeviationSubjectType
+import core.model.deviation.DeviationType
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.context.GlobalContext.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import kotlin.test.assertEquals
+
+internal class RelationNavigabilityTest : KoinTest {
+    private val controller by inject<Controller>()
+
+    private val testClassName = "RelationNavigability"
+
+    private val implClassA = "src/test/kotlin/testInput/relations/a/navigability/"
+    private val implClassB = "src/test/kotlin/testInput/relations/b/navigability/"
+
+    private val designClassA = "src/test/kotlin/testInput/relations/a/navigability/$testClassName.puml"
+    private val designClassB = "src/test/kotlin/testInput/relations/b/navigability/$testClassName.puml"
+
+    @Test
+    fun divergentAggregationRelation_reportsDeviation_ofTypeUnexpectedAbsent() {
+
+        val resultDeviationsA = controller.onExecuteCommandTest(implClassA, designClassB, TEST)
+        val resultDeviationsB = controller.onExecuteCommandTest(implClassB, designClassA, TEST)
+
+        assert(resultDeviationsA.any { deviation -> deviation.deviationType == DeviationType.UNEXPECTED })
+        assert(resultDeviationsA.any { deviation -> deviation.subjectType == DeviationSubjectType.RELATION })
+        assert(resultDeviationsA.any { deviation -> deviation.level == DeviationLevel.MAKRO })
+        assert(resultDeviationsA.any { deviation -> deviation.affectedClassesNames.contains(testClassName)})
+        assert(resultDeviationsA.any { deviation -> deviation.description.contains("NAVIGABILITY")})
+
+        assert(resultDeviationsB.any { deviation -> deviation.deviationType == DeviationType.ABSENT })
+        assert(resultDeviationsB.any { deviation -> deviation.subjectType == DeviationSubjectType.RELATION })
+        assert(resultDeviationsB.any { deviation -> deviation.level == DeviationLevel.MAKRO })
+        assert(resultDeviationsB.any { deviation -> deviation.affectedClassesNames.contains(testClassName)})
+        assert(resultDeviationsA.any { deviation -> deviation.description.contains("NAVIGABILITY")})
+
+    }
+
+    @Test
+    fun convergentAggregationRelation_reportsNoDeviation() {
+        assertEquals(emptyList(), controller.onExecuteCommandTest(implClassA, designClassA, TEST))
+        assertEquals(emptyList(), controller.onExecuteCommandTest(implClassB, designClassB, TEST))
+    }
+
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun startKoinForTest() {
+            startKoin {
+                modules(coreModule, controlModule)
+            }
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun stopKoinAfterTest(): Unit = stopKoin()
+    }
+}
---
+++ arcucheck/src/test/kotlin/testInput/relations/a/navigability/RelationNavigability.java
@@ -0,0 +1,9 @@
+package testInput.relations.a.navigability;
+
+public class RelationNavigability {
+    private TestClass testClass;
+
+    public RelationNavigability(TestClass testClass) {
+        this.testClass = testClass;
+    }
+}
